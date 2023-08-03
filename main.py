45adea707aa24a3f0d506a3254ba0cc60185bb3e
Linjie Xu (FA Talent) <v-linjiexu@microsoft.com>
2023-08-03 23:49:08 +0800

    test

--- main.py
+++ main.py
@@ -106,12 +106,12 @@ def train_agent(env, state_dim, action_dim, max_action, device, output_dir, args
                             np.mean(loss_metric['bc_loss']), np.mean(loss_metric['ql_loss']),
                             np.mean(loss_metric['actor_loss']), np.mean(loss_metric['critic_loss']),
                             curr_epoch])
-        results_dict = {"eval/reward":eval_res,
+        results_dict = {"eval/episodic_reward":eval_res,
                         "train/bc_loss":np.mean(loss_metric['bc_loss']),
-                        "ql_loss":np.mean(loss_metric['ql_loss']),
-                        "actor_loss":np.mean(loss_metric['actor_loss']),
-                        "critic_loss":np.mean(loss_metric['critic_loss']),
-                        "q_value":np.mean(loss_metric['q'])}
+                        "train/ql_loss":np.mean(loss_metric['ql_loss']),
+                        "train/actor_loss":np.mean(loss_metric['actor_loss']),
+                        "train/critic_loss":np.mean(loss_metric['critic_loss']),
+                        "train/q_value":np.mean(loss_metric['q'])}
         wandb.log(results_dict, step=training_iters)
         np.save(os.path.join(output_dir, "eval"), evaluations)
         logger.record_tabular('Average Episodic Reward', eval_res)
